c76322513e07bb7fb515eb0e8309117c2bb820e1
john-fotis <giannisfotis@gmail.com>
2022-04-19 21:53:43 +0300

    Custom Tooltip for both scenarios' markers

---
+++ src/components/TooltipContent.js
@@ -0,0 +1,28 @@
+import { Tooltip } from 'react-leaflet';
+
+export const circleTooltip = (sf, point) => {
+    return (
+      <Tooltip direction="right" opacity={1}>
+        <div>{`Spreading Factor: ${sf} `}</div>
+        <div>{`RSSI metrics: [${point.MinRSSI}, ${point.MaxRSSI}] dB`}</div>
+        <div>{`Average RSSI: ${point.AverageRSSI} dB`}</div>
+        <div>{`Median RSSI: ${point.MedianRSSI} dB`}</div>
+        <div>{`SNR metrics: [${point.MinSnr}, ${point.MaxSnr}] dB`}</div>
+        <div>{`Average SNR: ${point.AverageSNR} dB`}</div>
+        <div>{`Median SNR: ${point.MedianSNR} dB`}</div>
+        <div>{`Measurements: ${point.Measurements}`}</div>
+        <div>{`Packet Delivery Ratio: ${point.PDR}%`}</div>
+      </Tooltip>
+    );
+}
+
+export const maxRangeTooltip = (sf, point) => {
+    return (
+      <Tooltip direction="top" opacity={1}>
+        <div>{`Spreading Factor: ${sf} `}</div>
+        <div>{`RSSI: ${point.RSSI} dB`}</div>
+        <div>{`SNR: ${point.SNR} dB`}</div>
+        <div>{`Distance: ${point.Distance} m`}</div>
+      </Tooltip>
+    );
+}
