42f1886b03ca1b7bafa7f89477c36377d5a8e3e4
john-fotis <giannisfotis@gmail.com>
2022-04-19 21:53:06 +0300

    OpenStreetMap configuration parameters

---
+++ src/config/OSM.js
@@ -0,0 +1,23 @@
+var OSM = {
+  tiler: {
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    attribution:
+      '&copy; 2022 <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="https://github.com/john-fotis">John Fotis</a>, <a href="https://github.com/katerinagiann">Katerina Giannopoulou</a> | Hosted with &#10084; by <a href="https://github.com/john-fotis/lora-lorawan">Github</a>',
+    maxZoom: 19,
+    tileSize: 512,
+    zoomOffset: -1,
+    interaction: {
+      zoomControl: true,
+      doubleClickZoom: false,
+      closePopupOnClick: true,
+      dragging: true,
+      zoomSnap: false,
+      zoomDelta: false,
+      trackResize: false,
+      touchZoom: true,
+      scrollWheelZoom: true,
+    },
+  },
+};
+
+export default OSM;
