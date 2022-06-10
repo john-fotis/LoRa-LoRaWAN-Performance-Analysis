d6c49ea13d4b62e000109ad4451147f94947cbc1
John Fotis <giannisfotis@gmail.com>
2022-06-10 15:03:56 +0300

    Add legend title

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -18,14 +18,14 @@ function Legend({ map }) {
             legend.onAdd = () => {
                 const div = L.DomUtil.create("div", "info legend");
                 const grades = [-90, -95, -100, -105, -110, -115];
-                let labels = [];
+                let labels = ['<h4 style="display:flex;justify-content: center;height:6px;margin-block-start:0;margin-block-end:0;"><strong>RSSI level</strong></h4>'];
                 let level = 1;
 
                 // Fill the legend with entries for each RSSI level range
-                labels.push('<i style="background:' + getColor(grades[level]) + '">&nbsp&nbsp&nbsp&nbsp</i> &nbsp&gt; ' + grades[level] + 'dBm');
+                labels.push('<span style="background:' + getColor(grades[level]) + '">&nbsp&nbsp&nbsp&nbsp</span> &nbsp&gt; ' + grades[level] + 'dBm');
                 for (level = 1; level < grades.length - 1; level++)
-                    labels.push('<i style="background:' + getColor(grades[level + 1]) + '">&nbsp&nbsp&nbsp&nbsp</i> &nbsp' + grades[level] + (grades[level] ? "dBm to " + grades[level + 1] : "") + "dBm");
-                labels.push('<i style="background:' + getColor(grades[level + 1]) + '">&nbsp&nbsp&nbsp&nbsp</i> &nbsp&lt; ' + grades[level] + 'dBm');
+                    labels.push('<span style="background:' + getColor(grades[level + 1]) + '">&nbsp&nbsp&nbsp&nbsp</span> &nbsp' + grades[level] + (grades[level] ? "dBm to " + grades[level + 1] : "") + "dBm");
+                labels.push('<span style="background:' + getColor(grades[level + 1]) + '">&nbsp&nbsp&nbsp&nbsp</span> &nbsp&lt; ' + grades[level] + 'dBm');
 
                 div.innerHTML = labels.join("<br>");
                 return div;
